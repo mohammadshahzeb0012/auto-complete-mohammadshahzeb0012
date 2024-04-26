9a6f9a92fb52244c4bf670e201ea491487b90d17
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-04-26 13:11:52 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react"
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 
 const App = () => {
+  const [search,setSearch] = useState('')
+
+  const changeHandel = (e)=>{
+     setSearch(e.target.value)
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+      <div>
+          <h1>Search Item</h1>
+          <input type="text"  onChange={changeHandel}/>
+          <ul>
+              {search && fruits.filter(elem=> elem.indexOf(search) != -1)
+              .map(elem=> <li>{elem}</li>)
+              }
+          </ul>
+      </div>
   )
 }
 
